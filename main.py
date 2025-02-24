4d46a53ad5e80ab91891e732ecb07f39b4b3b136
ashen00ne <kaplunov2304@gmail.com>
2025-02-24 18:02:20 +0300

    pep8 standarts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from os import path
-import pygame
 import random
+import pygame
+from os import path
 
 img_dir = path.join(path.dirname(__file__), 'img')
 WIDTH = 480
@@ -22,15 +22,16 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = player_img
-        self.image = pygame.transform.scale(player_img,(70,70))
+        self.image = pygame.transform.scale(player_img, (70, 70))
         self.image.set_colorkey("RED")
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT -10
+        self.rect.bottom = HEIGHT - 10
         self.speedx = 0
 
     def update(self):
@@ -51,11 +52,12 @@ class Player(pygame.sprite.Sprite):
         all_sprites.add(bullet)
         bullets.add(bullet)
 
+
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = mob_img
-        self.image = pygame.transform.scale(mob_img,(50,50))
+        self.image = pygame.transform.scale(mob_img, (50, 50))
         self.image.set_colorkey("RED")
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(WIDTH - self.rect.width)
@@ -66,16 +68,19 @@ class Mob(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+        if (self.rect.top > HEIGHT + 10 or
+            self.rect.left < -25 or
+            self.rect.right > WIDTH + 20):
             self.rect.x = random.randrange(WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
             self.speedy = random.randrange(1, 8)
+
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = bullet_img
-        self.image = pygame.transform.scale(bullet_img,(20,30))
+        self.image = pygame.transform.scale(bullet_img, (20, 30))
         self.image.set_colorkey("RED")
         self.rect = self.image.get_rect()
         self.rect.bottom = y
@@ -84,12 +89,14 @@ class Bullet(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.y += self.speedy
-        
+
         if self.rect.bottom < 0:
             self.kill()
-bullet_img = pygame.image.load(path.join(img_dir,"patron.png")).convert()
-mob_img = pygame.image.load(path.join(img_dir,"igor.png")).convert()     
-player_img = pygame.image.load(path.join(img_dir,"player.png")).convert()
+
+
+bullet_img = pygame.image.load(path.join(img_dir, "patron.png")).convert()
+mob_img = pygame.image.load(path.join(img_dir, "igor.png")).convert()
+player_img = pygame.image.load(path.join(img_dir, "player.png")).convert()
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -103,18 +110,17 @@ for i in range(8):
 
 running = True
 while running:
-    
+
     clock.tick(FPS)
-   
+
     for event in pygame.event.get():
-        
+
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 player.shoot()
 
-  
     all_sprites.update()
 
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
@@ -133,5 +139,3 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
-
-
